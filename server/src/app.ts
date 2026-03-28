f453b771bc9c28fc47eed79e635fa1599a971ff3
Arijit Mondal <arijitm717@gmail.com>
2026-03-28 17:48:50 +0530

    fix rate limit

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,12 +16,10 @@ const app: Application = express();
 app.disable("x-powered-by");
 app.set("trust proxy", 1);
 
+app.use(helmet());
 app.use(corsOptions);
-
 app.use("/api/v1/auth", authLimiter);
 app.use("/api/v1",      apiLimiter);
-
-app.use(helmet());
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
